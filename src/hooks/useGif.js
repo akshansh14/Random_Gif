082122efe18abdb5b5ac45460f851d0a34ed06de
akshansh14 <akshanshsj14@gmail.com>
2024-08-03 21:46:51 +0530

    resolved build errors

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,27 +1,30 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export default function useGif(tag) {
   const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
   
   const [gif, setGif] = useState('');
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false);
 
-  
   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
-  
-  async function fetchData(tag) {
-    setLoading(true)
-    const { data } = await axios.get(tag? `${url}&tag=${tag}`:url);
-    const imageSource = data.data.images.downsized_large.url;
-    setGif(imageSource);
-    setLoading(false)
-  }
+
+  const fetchData = useCallback(async (tag) => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(tag ? `${url}&tag=${tag}` : url);
+      const imageSource = data.data.images.downsized_large.url;
+      setGif(imageSource);
+    } catch (error) {
+      console.error("Error fetching gif:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, [url]);
+
   useEffect(() => {
-    fetchData()
-  }, [fetchData]);
+    fetchData(tag);
+  }, [fetchData, tag]);
 
-
- return{gif,fetchData,loading}
- 
+  return { gif, fetchData, loading };
 }
